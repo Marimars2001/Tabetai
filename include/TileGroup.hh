f50b4435660f42244369210e33a71991b25118be
Marimars2001 <marimars2001@gmail.com>
2021-12-06 13:24:10 -0700

    publish

--- include/TileGroup.hh
+++ include/TileGroup.hh
@@ -1,15 +1,19 @@
 #pragma once
 #include "Tile.hh"
-#include <iostream>
+#include<iostream>
+#include<fstream>
 
 class TileGroup
 {
-    private:
-        void GenerateTiles();
-        std::vector<Tile*>* tiles;
-        sf::RenderWindow* window{};
-    public:
-        TileGroup(sf::RenderWindow* window, const char* textureUrl);
-        ~TileGroup();
-        void Draw();
+private:
+  std::ifstream* reader{};
+  int sizeX{}, sizeY{};
+  void GenerateTiles(sf::RenderWindow*& window, const char* textureUrl,
+  float tileWidth, float tileHeight, float tileScale, int sizeX, int sizeY, const char* tileGroupUrl);
+  std::vector<Tile*>* tiles;
+public:
+  TileGroup(sf::RenderWindow*& window, const char* textureUrl,
+  float tileWidth, float tileHeight, float tileScale, int sizeX, int sizeY, const char* tileGroupUrl);
+  ~TileGroup();
+  void Draw();
 };
